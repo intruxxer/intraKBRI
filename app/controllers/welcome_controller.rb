08cb07a7f1cf318cea5a973efa68638c02bb0db1
teguhsyahmar <teguh.syahmar@gmail.com>
2014-04-17 10:41:04 +0900

    merge intra with cloud 20140417

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,70 +1,55 @@
 class WelcomeController < ApplicationController
+   
   def index	
-    
     @ip_visitor = request.remote_ip
     session[:ip_address]  = @ip_visitor
     if !user_signed_in? then
       @visitorname = "Visitor"
+      #session[:warned_on_login] = 0
     else
       @visitorname = current_user.full_name
+      # session[:warned_on_login] += 1
+      # user_session[:test] = "User session upon logged in."
     end
     @visitor =  Visitor.new( who: @visitorname, ip_address: @ip_visitor.to_s, action: "Visiting Main Page")
     if @visitor.valid?
         @visitor.save!
-        message_one = "Dear "+ @visitorname.to_s + ",  Please kindly be notified that your IP address & Location is automatically 
-                       logged for security monitoring during your active access to E-KBRI."
-        message_two = "Your IP address is<b> #{@ip_visitor} </b>that is identified to be located in <b> #{@visitor.coordinates[0]} long </b> & <b> #{@visitor.coordinates[1]} lat </b>."
-        warning = [ message_one, message_two ]
-        flash[:warning] = warning.join("<br/>").html_safe
+        if !user_signed_in?
+          message_one = "Dear Visitor, Please kindly be notified that your IP address & Location is automatically 
+                         logged for security monitoring during your active access to E-KBRI."
+          message_two = "Your IP address is<b> #{@ip_visitor} </b>that is identified to be located in <b> #{@visitor.coordinates[0]}&deg; longitude </b> & <b> #{@visitor.coordinates[1]}&deg; latitude</b>."
+          warning = [ message_one, message_two ]
+          flash[:warning] = warning.join("<br/>").html_safe
+        end
                            
     end
                          
   	if user_signed_in?
   	  @userreport = Report.where(user_id: current_user.id).where(is_valid: true).desc(:updated_at)
   	  if current_user.has_role? :admin then
-  	    @uservisa = Visa.all
-  	    @userpassport = Passport.all
+  	    @uservisa = Visa.all.page(params[:page]).per(5)
+  	    @userpassport = Passport.all.page(params[:page]).per(5)
   	    
   	  else
   	    visadata = Visa.where(user_id: current_user)
 
-        @uservisa = Visa.where(user_id: current_user.id)
+        @uservisa = Visa.where(user_id: current_user.id).page(params[:page]).per(5)
         
         passportdata = Passport.where(user_id: current_user)
-        @userpassport = Passport.where(user_id: current_user.id)
+        @userpassport = Passport.where(user_id: current_user.id).page(params[:page]).per(5)
 
         
         
         allunpaidvisa = Visa.where(user_id: current_user, status: 'Received')
         @unpaidvisa = {}
-        if allunpaidvisa.count > 0
-           allunpaidvisa.each do |x|
-            @unpaidvisa[ "ref_id"=> x.ref_id, "category_type" => x.category_type,
-                         "first_name" => x.first_name, "last_name" => x.last_name ]
-            end
-        end
-  	  end
-=begin  	  	
-    		#if there is at least once visa/passport/report application, 
-    		#then visadata == 1 as for the current user
-    		if visadata.count > 0
-          @uservisa.each do |x|
-            puts x.inspect
+          if allunpaidvisa.count > 0
+             allunpaidvisa.each do |x|
+              @unpaidvisa[ "ref_id"=> x.ref_id, "category_type" => x.category_type,
+                           "first_name" => x.first_name, "last_name" => x.last_name ]
+              end
           end
-        end
-    		if passportdata.count > 0
-          @userpassport.each do |x|
-            puts x.inspect
-          end
-        end
-    		
-    		if @userreport.count > 0
-    			@userreport.each do |x|
-    				puts x.inspect
-    			end
-    		end
-=end    		
-    	end
+  	    end		
+    end
   end
   
   def showsamplebayar
